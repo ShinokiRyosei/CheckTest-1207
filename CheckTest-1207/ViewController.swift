ca089046c50ba63ffc125c33f017f116e7c978fc
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-12-07 00:17:34 +0900

    [Fix] answer

--- CheckTest-1207/ViewController.swift
+++ CheckTest-1207/ViewController.swift
@@ -11,14 +11,56 @@ import UIKit
 
 // MARK: - ViewController
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
+    
+    @IBOutlet var label: UILabel!
+    
+    @IBOutlet var textField: UITextField!
+    
+    let userDefaults: UserDefaults = UserDefaults.standard
     
     // MARK: UIViewController
 
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        textField.delegate = self
+    }
+    
+    @IBAction func selectSave(_ sender: UIButton) {
+        
+        // unwrap textField.text type String? to String
+        // textField.textをString? から Stringへアンラップ
+        if let text: String = textField.text {
+            
+            userDefaults.set(text, forKey: "text")
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        // unwrap userDefaults.string(forKey:) type String? to String
+        // userDefaults.string(forKey:)をString? から Stringへアンラップ
+        if let text: String = userDefaults.string(forKey: "text") {
+            
+            self.setOnLabel(withContentsOf: text)
+        }
+    }
+    
+    func setOnLabel(withContentsOf str: String) {
+        
+        label.text = str
+    }
+    
+    // Close keyboard if return key is tapped.
+    // Returキーでキーボードを閉じる
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
     }
 }
 
