e8353188c140f1afc7da227f26eb521893051c72
ShinokiRyosei <shinoki.r.0301@gmail.com>
2016-12-06 23:36:22 +0900

    initial commit

---
+++ CheckTest-1207/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CheckTest-1207
+//
+//  Created by ShinokiRyosei on 2016/12/06.
+//  Copyright © 2016年 ShinokiRyosei. All rights reserved.
+//
+
+import UIKit
+
+
+// MARK: - ViewController
+
+class ViewController: UIViewController {
+    
+    
+    // MARK: UIViewController
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+}
+
